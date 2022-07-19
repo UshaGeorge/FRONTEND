43b8d728ff99c4c54a547c4ee9fa1cd72726938c
UshaGeorge <usha.dm@gmail.com>
2022-07-19 20:35:26 +0530

    Initial Commit
    Frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { SampleComponent } from './sample/sample.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { ServicesComponent } from './services/services.component';
+import { TestimonyComponent } from './testimony/testimony.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    SampleComponent,
+    ContactComponent,
+    HomeComponent,
+    LandingpageComponent,
+    ServicesComponent,
+    TestimonyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
